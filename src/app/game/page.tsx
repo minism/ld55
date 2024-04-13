3275b68fd58bc4f91ee6a4f9631f71399f764d30
josh <joshbothun@gmail.com>
2024-04-12 22:33:08 -0700

    Show username

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,10 +1,13 @@
 import GameContainer from "@/components/GameContainer";
+import { getAuthenticatedSupabaseOrRedirect } from "@/supabase/server";
 
-export default function Game() {
+export default async function Game() {
+  const { user, supabase } = await getAuthenticatedSupabaseOrRedirect();
+
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <main className="flex min-h-screen flex-col items-center p-16">
-      <h1 className="text-xl">LD55</h1>
+      <h1 className="text-xl my-4">Playing as {user.user_metadata.username}</h1>
       <GameContainer />
     </main>
   );
--- src/components/GameContainer.tsx
+++ src/components/GameContainer.tsx
@@ -15,7 +15,7 @@ export default function GameContainer() {
   }, [gameElement]);
 
   return (
-    <div>
+    <div className="border-white border">
       <div ref={gameElement} />
     </div>
   );
